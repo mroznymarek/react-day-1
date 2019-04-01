85c9c25f8619f3f359b41f2b465a7b0e2a6515a9
Marek Mroz <marekmroz@wp.pl>
2019-04-01 13:20:58 +0200

    add new component DisplayName

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import DisplayPrefix from './DisplayPrefix'
+import DisplayName from './DisplayName'
 
 const App = () => (
   <div>
-  <h1>Hello</h1>
+    <h1>Hello</h1>
+    <DisplayPrefix />
+    <DisplayName />
   </div>
-  )
+)
 
 export default App;
---
+++ src/DisplayName.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayName = () => (
+    <div>
+        <h1>Marek Mróz</h1>
+    </div>
+)
+
+export default DisplayName
