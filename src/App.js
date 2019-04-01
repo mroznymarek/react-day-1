7a83b477f921834a447b829d2b0584651843d07c
Marek Mroz <marekmroz@wp.pl>
2019-04-01 14:02:22 +0200

    what react can display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import DisplayPrefix from './DisplayPrefix'
 import DisplayName from './DisplayName'
+import WhatReactCanDisplay from './WhatReactCanDisplay';
 
 const App = () => (
   <div>
     <h1>Hello</h1>
     <DisplayPrefix />
     <DisplayName />
+    <WhatReactCanDisplay />
   </div>
 )
 
---
+++ src/WhatReactCanDisplay.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const reactCanDisplayString = 'Ala ma kota'
+const reactCanDisplayNumbers = 12345
+const reactCantDisplayBolean = true
+const reactCantDislplayNull = null
+const reactCantDislplayUndefined = undefined
+
+const reactCanDisplayFunctionCall = () => 'Ala ma psa'
+
+const reactCanDisplayArray = ['Ala', 'Ola', 'Ela']
+
+const reactCanDisplayReactElement = <b>Gruba Ala</b>
+
+const WhatReactCanDisplay = (props) => (
+    <div>
+        <h4>Strings:</h4>
+            <p>{reactCanDisplayString}</p>
+        <h4>Numbers:</h4>
+            <p>{reactCanDisplayNumbers}</p>
+        <h4>Boleans:</h4>
+            <p>{reactCantDisplayBolean}</p>
+        <h4>Null:</h4>
+            <p>{reactCantDislplayNull}</p>
+        <h4>Undefined:</h4>
+            <p>{reactCantDislplayUndefined}</p>
+        <h4>Function Call:</h4>
+            <p>{reactCanDisplayFunctionCall()}</p>
+        <h4>Array:</h4>
+            <p>{reactCanDisplayArray}</p>
+        <h4>React Element:</h4>
+            <p>{reactCanDisplayReactElement}</p>
+        <h4>Array mapping:</h4>
+            <p>{reactCanDisplayArray.map(
+                element => <li>{element}</li>
+            )
+            }</p>
+    </div>
+)
+
+export default WhatReactCanDisplay
